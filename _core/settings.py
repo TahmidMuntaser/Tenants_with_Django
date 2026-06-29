2e65f6bb0460d3f7a24ab8c33312f6e3c8b371e9
Tahmid <115565668+TahmidMuntaser@users.noreply.github.com>
2026-06-29 16:44:12 +0600

    feat: items list feature

--- _core/settings.py
+++ _core/settings.py
@@ -95,8 +95,13 @@ WSGI_APPLICATION = '_core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5433',
+        
     }
 }
 
